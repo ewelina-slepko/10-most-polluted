a1a686dfe1dacaa2707a70bc330edc018e09acab
ewelina-slepko <slepko.ewelina@gmail.com>
2019-08-11 12:11:56 +0200

    Updates

---
+++ precache-manifest.a6d3a5ddc004eb38988418f033c97c2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0863ce7f71466e6b9fd2a867118b1f11",
+    "url": "/ewelina-slepko/10-most-polluted/index.html"
+  },
+  {
+    "revision": "b243e88fcac07e17a329",
+    "url": "/ewelina-slepko/10-most-polluted/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "580726bd774c8bd0b67f",
+    "url": "/ewelina-slepko/10-most-polluted/static/js/2.4894f407.chunk.js"
+  },
+  {
+    "revision": "b243e88fcac07e17a329",
+    "url": "/ewelina-slepko/10-most-polluted/static/js/main.8bfb974a.chunk.js"
+  },
+  {
+    "revision": "8c9f99e087f1dbc714ad",
+    "url": "/ewelina-slepko/10-most-polluted/static/js/runtime~main.165790da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ewelina-slepko/10-most-polluted/static/media/logo.5d5d9eef.svg"
+  }
+]);
