69af195791857da050a0b2e6287abbca2bd3fc25
ewelina.slepko <slepko.ewelina@gmail.com>
2019-08-11 13:49:31 +0200

    Fetched the data

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,13 +1,40 @@
 import React from 'react'
 import Info from './Info'
+import styles from '../styles/Dashboard.module.css'
 
 class Dashboard extends React.Component {
+    state = {
+        cities: '',
+        currentCountry: ''
+    }
+    onChange = (e) => {
+        e.preventDefault()
+        console.log(e.target.value)
+        this.setState({ currentCountry: e.target.value })
+    }
+    componentDidMount() {
+        fetch('https://api.openaq.org/v1/locations')
+            .then(response => response.json())
+            .then(json => this.setState({ cities: json }));
+    }
     render() {
+        const countries = ["Poland", "Germany", "Spain", "France"]
         return (
-            <>
-                <div>Hello!</div>
+            <div className={styles.container}>
+                <h3>Enter the country</h3>
+                <form>
+                    <input type="text" id="txtAutoComplete" list="languageList" onChange={this.onChange} className={styles.input_style} />
+                    <datalist id="languageList">
+                        {countries.map((country) => {
+                            return (
+                                <option value={country} key={country} className={styles.select_style}>{country}</option>
+                            )
+                        })}
+                    </datalist>
+                </form>
+
                 <Info />
-            </>
+            </div>
         )
     }
 }
