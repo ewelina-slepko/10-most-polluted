baeb3a88eaab3221e555fbefdfca0b8d7cce0765
ewelina.slepko <slepko.ewelina@gmail.com>
2019-08-11 12:36:49 +0200

    Created basic structure of the application

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Info from './Info'
+
+class Dashboard extends React.Component {
+    render() {
+        return (
+            <>
+                <div>Hello!</div>
+                <Info />
+            </>
+        )
+    }
+}
+
+export default Dashboard;
