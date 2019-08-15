b15cedd70303ae1058b6d13aac5636c0debf6a19
ewelina.slepko <slepko.ewelina@gmail.com>
2019-08-15 12:32:11 +0200

    Included countries code in array and transfered data to render

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -6,23 +6,11 @@ class Dashboard extends React.Component {
     state = {
         cities: '',
         currentCountry: '',
-        countries: ''
-    }
-    componentDidMount() {
-        fetch('https://api.openaq.org/v1/cities?order_by=country&sort=asc&limit=10000')
-            .then(response => response.json())
-            .then(json => this.setState({ countries: json }));
     }
     getInfo = (shortcut) => {
         fetch(`https://api.openaq.org/v1/measurements?country=${shortcut}&parameter=pm25&order_by=value&sort=desc&limit=10000`)
             .then(response => response.json())
             .then(json => this.setState({ cities: json }));
-    }
-    getUniqueCountry = () => {
-        const countries = this.state.countries && this.state.countries.results.map((info) => {
-            return info.country
-        })
-        return countries
     }
     sortCities = () => {
         let arr = []
@@ -48,21 +36,31 @@ class Dashboard extends React.Component {
         this.getInfo(e.target.options[e.target.selectedIndex].text)
     }
     render() {
+        const countries = ['BE', 'BA', 'HR', 'CZ', 'FI', 'FR', 'GE',
+            'DE', 'HU', 'IE', 'IT', 'XK', 'LT', 'LU', 'MK', 'MT', 'NO', 'PL',
+            'PT', 'RS', 'SK', 'ES', 'CH', 'GB']
         return (
             <div className={styles.container}>
                 <h3>Select the country</h3>
                 <form>
                     <select id="countrySelect" onChange={this.onChange}>
-                        {[...new Set(this.getUniqueCountry())].map((country) => {
+                        {countries.map((country) => {
                             return <option value={country} key={country}>{country}</option>
                         })}
                     </select>
                 </form>
                 <Info />
                 {this.sortCities().map((info) => {
-                    return <p>{info.city}</p>
+                    return (
+                        <>
+                            <p>
+                                <span>City: {info.city} </span>
+                                <span>Value: {info.value} </span>
+                                <span>Parameter: {info.parameter} </span>
+                            </p>
+                        </>
+                    )
                 })}
-
             </div>
         )
     }
